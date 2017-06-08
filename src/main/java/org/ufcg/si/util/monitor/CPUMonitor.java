a91474ca3920c1ebbaea541da2626e68a0736fba
Benardi <jose.nunes@ccc.ufcg.edu.br>
2017-06-08 11:47:16 -0300

    Suppressing warnings for CPU monitor class

--- src/main/java/org/ufcg/si/util/monitor/CPUMonitor.java
+++ src/main/java/org/ufcg/si/util/monitor/CPUMonitor.java
@@ -3,6 +3,7 @@ package org.ufcg.si.util.monitor;
 import java.lang.management.ManagementFactory;
 import com.sun.management.OperatingSystemMXBean;
 
+@SuppressWarnings("restriction")
 public class CPUMonitor {
 	private OperatingSystemMXBean osBean;
 
@@ -13,28 +14,21 @@ public class CPUMonitor {
 	public double getUsage() {
 		double load = -1;
 
-        for(int i=0; i<10; i++) {
-            load = osBean.getSystemCpuLoad();
-            System.out.println(load);
-            if((load<0.0 || load>1.0) && load != -1.0) {
-                throw new RuntimeException("getSystemCpuLoad() returns " + load
-                       +  " which is not in the [0.0,1.0] interval");
-            }
-            try {
-                Thread.sleep(200);
-            } catch(InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		for (int i = 0; i < 10; i++) {
+			load = osBean.getSystemCpuLoad();
+			System.out.println(load);
+			if ((load < 0.0 || load > 1.0) && load != -1.0) {
+				throw new RuntimeException(
+						"getSystemCpuLoad() returns " + load + " which is not in the [0.0,1.0] interval");
+			}
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 
 		return load;
-	}
-
-	public static void main(String[] args) {
-		CPUMonitor cp = new CPUMonitor();
-		System.out.println(cp.getUsage());
-
 	}
 
 }
