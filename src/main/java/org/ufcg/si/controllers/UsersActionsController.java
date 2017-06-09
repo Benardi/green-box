4ce569f2b3f654f71973bf48054892a498d734b6
Benardi <jose.nunes@ccc.ufcg.edu.br>
2017-06-09 01:26:43 -0300

    Instrumentation of Controller operations ADD and DEL files

--- src/main/java/org/ufcg/si/controllers/UsersActionsController.java
+++ src/main/java/org/ufcg/si/controllers/UsersActionsController.java
@@ -25,13 +25,12 @@ import org.ufcg.si.models.User;
 import org.ufcg.si.repositories.UserService;
 import org.ufcg.si.repositories.UserServiceImpl;
 import org.ufcg.si.util.ServerConstants;
+import org.ufcg.si.util.monitor.MeasurementDevice;
 import org.ufcg.si.util.permissions.file.FilePermissions;
 
-
 /**
- * This controller class uses JSON data format to be the 
- * endpoint of requests related to user actions
- * on the server-side.
+ * This controller class uses JSON data format to be the endpoint of requests
+ * related to user actions on the server-side.
  * 
  * <strong>This controller needs to a valid token to be accessed.</strong>
  */
@@ -39,40 +38,40 @@ import org.ufcg.si.util.permissions.file.FilePermissions;
 @RequestMapping(ServerConstants.SERVER_REQUEST_URL + ServerConstants.USERS_ACTIONS_REQUEST_URL)
 public class UsersActionsController {
 	private UserService userService;
-	
+
 	/**
 	 * Updates the userService, which works as a repository
+	 * 
 	 * @param userServiceImpl
-	 * 		The new user service
+	 *            The new user service
 	 */
 	@Autowired
 	public void setUserService(UserServiceImpl userServiceImpl) {
 		this.userService = userServiceImpl;
 	}
-	
+
 	/**
 	 * Creates a new folder in a certain folder
+	 * 
 	 * @param body
-	 * 		All the information necessary to add a new folder
+	 *            All the information necessary to add a new folder
 	 * @return the http status and user updated
-	 * @throws ServletException if can't create the folder
+	 * @throws ServletException
+	 *             if can't create the folder
 	 */
-	@RequestMapping(value = "/newfolder", 
-					method = RequestMethod.POST,
-					produces = MediaType.APPLICATION_JSON_VALUE,
-					consumes = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/newfolder", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<User> addFolder(@RequestBody AddNDeleteFolderBean body) throws ServletException {
 		try {
 			ExceptionHandler.checkAddFolderBody(body);
-		
+
 			User dbUser = userService.findByUsername(body.getUser().getUsername());
 			ExceptionHandler.checkUserInDatabase(dbUser);
 
 			dbUser.addFolder(body.getFolderName(), body.getFolderPath());
 			User updatedUser = userService.update(dbUser);
-		
+
 			return new ResponseEntity<>(updatedUser, HttpStatus.OK);
-		} catch(GreenboxException gbe) {
+		} catch (GreenboxException gbe) {
 			gbe.printStackTrace();
 			throw new ServletException("Request error while trying to create new folder... " + gbe.getMessage());
 		} catch (DataAccessException dae) {
@@ -80,76 +79,80 @@ public class UsersActionsController {
 			throw new ServletException("Request error while trying to create new folder... " + dae.getMessage());
 		}
 	}
-	
 
 	/**
 	 * Creates a new file in a certain folder
+	 * 
 	 * @param body
-	 * 		All the information necessary to add a new file
+	 *            All the information necessary to add a new file
 	 * @return the http status and user updated
-	 * @throws ServletException if can't create the file
+	 * @throws ServletException
+	 *             if can't create the file
 	 */
-	@RequestMapping(value = "/newfile", 
-					method = RequestMethod.POST,
-					produces = MediaType.APPLICATION_JSON_VALUE,
-					consumes = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/newfile", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<User> addFile(@RequestBody AddNDeleteFileBean body) throws ServletException {
+		MeasurementDevice mdADD = new MeasurementDevice();
+		mdADD.startMeasurement();
 		try {
 			ExceptionHandler.checkAddFileBody(body);
-			
+
 			User dbUser = userService.findByUsername(body.getUser().getUsername());
 			ExceptionHandler.checkUserInDatabase(dbUser);
-			
+
 			dbUser.addFile(body.getFileName(), body.getFileExtension(), body.getFileContent(), body.getFilePath());
 			User updatedUser = userService.update(dbUser);
-			
+			mdADD.endMeasurement();
+			mdADD.logMeasurement("ADD");
 			return new ResponseEntity<>(updatedUser, HttpStatus.OK);
-		} catch(GreenboxException gbe) {
+		} catch (GreenboxException gbe) {
 			gbe.printStackTrace();
 			throw new ServletException("Request error while trying to create new file... " + gbe.getMessage());
 		} catch (DataAccessException dae) {
 			dae.printStackTrace();
 			throw new ServletException("Request error while trying to create new file... " + dae.getMessage());
 		}
+
 	}
-	
+
 	/**
 	 * Edits a certain file
+	 * 
 	 * @param requestBody
-	 * 		All the necessary information to find and edit a file
+	 *            All the necessary information to find and edit a file
 	 * @return the http status and user updated.
-	 * @throws Exception if can't edit the file
+	 * @throws Exception
+	 *             if can't edit the file
 	 */
-	@RequestMapping(value = "/editfile",
-					method = RequestMethod.PUT,
-					produces = MediaType.APPLICATION_JSON_VALUE,
-					consumes = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<User> editFile(@RequestBody EditFileBean requestBody) throws Exception{
+	@RequestMapping(value = "/editfile", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<User> editFile(@RequestBody EditFileBean requestBody) throws Exception {
 		try {
 			ExceptionHandler.checkEditFileBody(requestBody);
-			
+
 			User dbUser = userService.findByUsername(requestBody.getUser().getUsername());
 			ExceptionHandler.checkUserInDatabase(dbUser);
-			
+
 			if (!requestBody.getFileName().equals(requestBody.getClickedFile().getName())) {
-				dbUser.editFileName(requestBody.getFileName(), requestBody.getClickedFile().getName(), requestBody.getClickedFile().getExtension(), requestBody.getFilePath());
+				dbUser.editFileName(requestBody.getFileName(), requestBody.getClickedFile().getName(),
+						requestBody.getClickedFile().getExtension(), requestBody.getFilePath());
 			}
-			
+
 			if (!requestBody.getFileExtension().equals(requestBody.getClickedFile().getExtension())) {
-				dbUser.editFileExtension(requestBody.getFileExtension(), requestBody.getFileName(), requestBody.getClickedFile().getExtension(), requestBody.getFilePath());
+				dbUser.editFileExtension(requestBody.getFileExtension(), requestBody.getFileName(),
+						requestBody.getClickedFile().getExtension(), requestBody.getFilePath());
 			}
-			
+
 			if (!requestBody.getFileContent().equals(requestBody.getClickedFile().getContent())) {
-				dbUser.editFileContent(requestBody.getFileName(), requestBody.getFileContent(), requestBody.getFileExtension(), requestBody.getFilePath());
+				dbUser.editFileContent(requestBody.getFileName(), requestBody.getFileContent(),
+						requestBody.getFileExtension(), requestBody.getFilePath());
 			}
-			
+
 			User updateUser = userService.update(dbUser);
-			
-			long used  = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+
+			long used = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
 			updateUser.setMemoryUsage(used);
-			
+
 			return new ResponseEntity<>(updateUser, HttpStatus.OK);
-		} catch(GreenboxException gbe) {
+		} catch (GreenboxException gbe) {
 			gbe.printStackTrace();
 			throw new ServletException("Request error while trying to edit file... " + gbe.getMessage());
 		} catch (DataAccessException dae) {
@@ -157,201 +160,200 @@ public class UsersActionsController {
 			throw new ServletException("Request error while trying to edit file... " + dae.getMessage());
 		}
 	}
-	
+
 	/**
 	 * Rename a certain folder
+	 * 
 	 * @param requestBody
-	 * 		All the necessary information to find and rename a folder
+	 *            All the necessary information to find and rename a folder
 	 * @return the http status and user updated.
-	 * @throws Exception if can't rename the folder
+	 * @throws Exception
+	 *             if can't rename the folder
 	 */
-	@RequestMapping(value = "/renamefolder", 
-					method = RequestMethod.PUT,
-					produces = MediaType.APPLICATION_JSON_VALUE,
-					consumes = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<User> renameFolder(@RequestBody RenameFolderBean body)throws Exception{
+	@RequestMapping(value = "/renamefolder", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<User> renameFolder(@RequestBody RenameFolderBean body) throws Exception {
 		try {
 			ExceptionHandler.checkRenameFolderBody(body);
 			User dbUser = userService.findByUsername(body.getUser().getUsername());
-		
+
 			ExceptionHandler.checkUserInDatabase(dbUser);
-			
+
 			dbUser.editFolderName(body.getNewName(), body.getOldName(), body.getFolderPath());
 			User updateUser = userService.update(dbUser);
-		
+
 			return new ResponseEntity<>(updateUser, HttpStatus.OK);
-		} catch(GreenboxException gbe) {
+		} catch (GreenboxException gbe) {
 			gbe.printStackTrace();
 			throw new ServletException("Request error while trying to rename folder... " + gbe.getMessage());
 		} catch (DataAccessException dae) {
 			dae.printStackTrace();
 			throw new ServletException("Request error while trying to rename folder... " + dae.getMessage());
-		} 
+		}
 	}
-	
+
 	/**
 	 * Rename a certain file
+	 * 
 	 * @param requestBody
-	 * 		All the necessary information to find and rename a file
+	 *            All the necessary information to find and rename a file
 	 * @return the http status and user updated.
-	 * @throws Exception if can't rename the file
+	 * @throws Exception
+	 *             if can't rename the file
 	 */
-	@RequestMapping(value = "/renamefile", 
-			method = RequestMethod.PUT,
-			produces = MediaType.APPLICATION_JSON_VALUE,
-			consumes = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/renamefile", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<User> renameFile(@RequestBody RenameFileBean body) throws ServletException {
 		try {
 			ExceptionHandler.checkRenameFileBody(body);
-			
+
 			User dbUser = userService.findByUsername(body.getUser().getUsername());
 			ExceptionHandler.checkUserInDatabase(dbUser);
-			
+
 			dbUser.editFileName(body.getNewName(), body.getOldName(), body.getFileExtension(), body.getFolderPath());
 			User updateUser = userService.update(dbUser);
-		
+
 			return new ResponseEntity<>(updateUser, HttpStatus.OK);
-		} catch(GreenboxException gbe) {
+		} catch (GreenboxException gbe) {
 			gbe.printStackTrace();
 			throw new ServletException("Request error while trying to rename folder... " + gbe.getMessage());
 		} catch (DataAccessException dae) {
 			dae.printStackTrace();
 			throw new ServletException("Request error while trying to rename folder... " + dae.getMessage());
-		} 
+		}
 	}
-	
+
 	/**
 	 * Share a certain file
+	 * 
 	 * @param requestBody
-	 * 		All the necessary information to find and share a file
+	 *            All the necessary information to find and share a file
 	 * @return the http status and user updated.
-	 * @throws Exception if can't share the file
+	 * @throws Exception
+	 *             if can't share the file
 	 */
-	@RequestMapping(value = "/sharefile", 
-			method = RequestMethod.POST,
-			produces = MediaType.APPLICATION_JSON_VALUE,
-			consumes = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/sharefile", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<User> shareFile(@RequestBody ShareFileBean body) throws Exception {
 		try {
 			ExceptionHandler.checkShareFileBody(body);
-			
+
 			User sendingUser = userService.findByUsername(body.getUser().getUsername());
 			User receivingUser = userService.findByUsername(body.getUserSharedWith().getUsername());
-		
+
 			ExceptionHandler.checkUserInDatabase(sendingUser);
 			ExceptionHandler.checkUserInDatabase(receivingUser);
-			
-			sendingUser.shareFile(receivingUser, body.getName(), body.getFolderPath(), body.getFileExtension(), FilePermissions.valueOfIgnoreCase(body.getPermissionLevel()));
+
+			sendingUser.shareFile(receivingUser, body.getName(), body.getFolderPath(), body.getFileExtension(),
+					FilePermissions.valueOfIgnoreCase(body.getPermissionLevel()));
 			User updateUser = userService.update(sendingUser);
 			userService.update(receivingUser);
-		
+
 			return new ResponseEntity<>(updateUser, HttpStatus.OK);
-		} catch(GreenboxException gbe) {
+		} catch (GreenboxException gbe) {
 			gbe.printStackTrace();
 			throw new ServletException("Request error while trying to share file... " + gbe.getMessage());
 		} catch (DataAccessException dae) {
 			dae.printStackTrace();
 			throw new ServletException("Request error while trying to share file... " + dae.getMessage());
-		} 
+		}
 	}
-	
+
 	/**
 	 * Show the user's notification
+	 * 
 	 * @param requestBody
-	 * 		All the information necessary to show the user's notification
+	 *            All the information necessary to show the user's notification
 	 * @return the http status and user updated.
-	 * @throws Exception if can't show the notifications
+	 * @throws Exception
+	 *             if can't show the notifications
 	 */
-	@RequestMapping(value = "/visit_notifications", 
-			method = RequestMethod.PUT,
-			produces = MediaType.APPLICATION_JSON_VALUE,
-			consumes = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/visit_notifications", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<User> visitNotifications(@RequestBody User requestBody) throws Exception {
 		try {
 			User dbUser = userService.findByUsername(requestBody.getUsername());
-			
+
 			ExceptionHandler.checkUserInDatabase(dbUser);
 			Iterable<Notification> notifications = dbUser.listNotifications();
-			
+
 			for (Notification notification : notifications) {
 				notification.setIsVisited(true);
 			}
-			
+
 			User updateUser = userService.update(dbUser);
-			
+
 			return new ResponseEntity<>(updateUser, HttpStatus.OK);
-		} catch(GreenboxException gbe) {
+		} catch (GreenboxException gbe) {
 			gbe.printStackTrace();
 			throw new ServletException("Request error while trying to rename folder... " + gbe.getMessage());
 		} catch (DataAccessException dae) {
 			dae.printStackTrace();
 			throw new ServletException("Request error while trying to rename folder... " + dae.getMessage());
-		} 
+		}
 	}
-	
+
 	/**
 	 * Delete a file
+	 * 
 	 * @param requestBody
-	 * 		All the information necessary to delete a file
+	 *            All the information necessary to delete a file
 	 * @return the http status and user updated.
-	 * @throws Exception if can't delete the file
+	 * @throws Exception
+	 *             if can't delete the file
 	 */
-	
-	@RequestMapping(value = "/deletefile", 
-			method = RequestMethod.PUT,
-			produces = MediaType.APPLICATION_JSON_VALUE,
-			consumes = MediaType.APPLICATION_JSON_VALUE)
-		public ResponseEntity<User> deleteFile(@RequestBody AddNDeleteFileBean requestBody)throws Exception {
-			try {
-				User dbUser = userService.findByUsername(requestBody.getUser().getUsername());
-				
-				ExceptionHandler.checkUserInDatabase(dbUser);
-				dbUser.deleteFile(requestBody.getFileName(), requestBody.getFileExtension(), requestBody.getFilePath());
-				User updateUser = userService.update(dbUser);
-				
-				long used  = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-				updateUser.setMemoryUsage(used);
-				
-				return new ResponseEntity<>(updateUser, HttpStatus.OK);
-			} catch(GreenboxException gbe) {
-				gbe.printStackTrace();
-				throw new ServletException("Request error while trying to delete file..." + gbe.getMessage());
-			} catch(DataAccessException dae) {
-				dae.printStackTrace();
-				throw new ServletException("Request error while trying to delete file... " + dae.getMessage());
-			}
-		
-		}
-	
-	/**
-	 * Delete a folder
-	 * @param requestBody
-	 * 		All the information necessary to delete a folder
-	 * @return the http status and user updated.
-	 * @throws Exception if can't delete the folder
-	 */
-	
-	@RequestMapping(value = "/deletefolder",
-			method = RequestMethod.PUT,
-			produces = MediaType.APPLICATION_JSON_VALUE,
-			consumes = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<User> deleteFolder(@RequestBody AddNDeleteFolderBean requestBody)throws Exception {
+
+	@RequestMapping(value = "/deletefile", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<User> deleteFile(@RequestBody AddNDeleteFileBean requestBody) throws Exception {
+		MeasurementDevice mdDEL = new MeasurementDevice();
+		mdDEL.startMeasurement();
 		try {
 			User dbUser = userService.findByUsername(requestBody.getUser().getUsername());
-			
+
+			ExceptionHandler.checkUserInDatabase(dbUser);
+			dbUser.deleteFile(requestBody.getFileName(), requestBody.getFileExtension(), requestBody.getFilePath());
+			User updateUser = userService.update(dbUser);
+
+			long used = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+			updateUser.setMemoryUsage(used);
+			mdDEL.endMeasurement();
+			mdDEL.logMeasurement("DEL");
+
+			return new ResponseEntity<>(updateUser, HttpStatus.OK);
+		} catch (GreenboxException gbe) {
+			gbe.printStackTrace();
+			throw new ServletException("Request error while trying to delete file..." + gbe.getMessage());
+		} catch (DataAccessException dae) {
+			dae.printStackTrace();
+			throw new ServletException("Request error while trying to delete file... " + dae.getMessage());
+		}
+
+	}
+
+	/**
+	 * Delete a folder
+	 * 
+	 * @param requestBody
+	 *            All the information necessary to delete a folder
+	 * @return the http status and user updated.
+	 * @throws Exception
+	 *             if can't delete the folder
+	 */
+
+	@RequestMapping(value = "/deletefolder", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<User> deleteFolder(@RequestBody AddNDeleteFolderBean requestBody) throws Exception {
+		try {
+			User dbUser = userService.findByUsername(requestBody.getUser().getUsername());
+
 			ExceptionHandler.checkUserInDatabase(dbUser);
 
 			dbUser.deleteFolder(requestBody.getFolderPath(), requestBody.getFolderName());
 			System.out.println(dbUser);
 			User updateUser = userService.update(dbUser);
 			return new ResponseEntity<>(updateUser, HttpStatus.OK);
-		}  catch(GreenboxException gbe) {
+		} catch (GreenboxException gbe) {
 			gbe.printStackTrace();
 			throw new ServletException("Request error while trying to delete file..." + gbe.getMessage());
-		} catch(DataAccessException dae) {
+		} catch (DataAccessException dae) {
 			dae.printStackTrace();
 			throw new ServletException("Request error while trying to delete file... " + dae.getMessage());
 		}
 	}
-	
+
 }
